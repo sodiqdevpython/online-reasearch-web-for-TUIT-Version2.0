a7f16b4840f2a782ae97f59752100c07e9b61431
Sodiq <sodiq7898@gmail.com>
2023-11-13 20:45:17 +0500

    disallowed host solved

--- config/settings.py
+++ config/settings.py
@@ -21,7 +21,7 @@ SECRET_KEY = 'django-insecure-&^(4v(kqfs37k_m$dr5j1qcns7z(2q=7=o#*b8=@h_vi2yk606
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.pythonanywhere','127.0.0.1']
+ALLOWED_HOSTS = ['.pythonanywhere.com','127.0.0.1']
 
 # Application definition
 
